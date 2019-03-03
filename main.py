b53beff1d87bab71ff2f965f8a4f5651874dc1c2
blizzard <s41.blizzard@mail.ru>
2019-03-03 17:18:19 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	employees.sqlite

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import sqlite3
 import smtplib
+import schedule
+import time
 
-conn = sqlite3.connect('employees.sqlite')
 
-cursor = conn.cursor()
+def checking_birthdays():
+    conn = sqlite3.connect('employees.sqlite')
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT id, LastName, FirstName from employees WHERE strftime('%m',DOB) = strftime('%m','now') AND  strftime('%d','now') = strftime('%d', DOB)")
 
-cursor.execute(
-    "SELECT id, LastName, FirstName from employees WHERE strftime('%m',DOB) = strftime('%m','now') AND  strftime('%d','now') = strftime('%d', DOB)")
+    results = cursor.fetchall()
 
-results = cursor.fetchall()
+    # print(results[0][1])
+    conn.close()
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.starttls()
+    smtpObj.login('zard.41@gmail.com', 'gravity84')
+    for empl in results:
+        text = "Поздавляем сотрудника " + empl[1] + " " + empl[2] + " с Днем Рождения!"
+        text_encoded = text.encode('utf-8').strip()
+        smtpObj.sendmail("zard.41@gmail.com", "s41.blizzard@mail.ru", text_encoded)
 
-# print(results[0][1])
-conn.close()
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj.starttls()
-smtpObj.login('zard.41@gmail.com', 'gravity84')
-text = "Поздавляем сотрудника " + results[0][1] + " " + results[0][2] + " с Днем Рождения!"
-text_encoded = text.encode('utf-8').strip()
-smtpObj.sendmail("zard.41@gmail.com", "s41.blizzard@mail.ru", text_encoded)
 
+schedule.every().day.at("09:00").do(checking_birthdays())
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 # текущая день месяц strftime('%m','now'), strftime('%d','now')
 # запрос для др "SELECT id, LastName, FirstName from employees WHERE strftime('%m',DOB) = strftime('%m','now') AND  strftime('%d','now') = strftime('%d', DOB)"
 ###
