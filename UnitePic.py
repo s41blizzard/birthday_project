96025c7e5c17a28c927f1f10d3d57d9c730cdd64
blizzard <s41.blizzard@mail.ru>
2019-03-08 23:32:36 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	employees.sqlite

--- UnitePic.py
+++ UnitePic.py
@@ -8,10 +8,7 @@ background = background.convert('RGBA')
 bwidth, bheight = background.size[0], background.size[1]
 fwidth, fheight = foreground.size[0], foreground.size[1]
 x, y = int((bwidth / 2) - (fwidth / 2)), int((bheight / 2) - (fheight / 2))  # центрирование
-x, y = 0, 0  # в левый верхний
-x, y = 0, bheight - fheight  # в левый нижний
-x, y = bwidth - fwidth, bheight - fheight  # в правый нижний
-x, y = bwidth - fwidth, 0  # в правый верхний
 
-background.paste(foreground, (0, 0), mask=foreground)
+
+background.paste(foreground, (x, y), mask=foreground)
 background.show()
