b6d938c050b37c17fd193cbba9dd872447434cec
blizzard <s41.blizzard@mail.ru>
2019-03-08 23:50:07 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	employees.sqlite

--- zup_parser.py
+++ zup_parser.py
@@ -5,13 +5,18 @@ conn = sqlite3.connect('employees.sqlite')
 tree = ET.parse("Message_ЗК_FL.xml")
 root = tree.getroot()
 cursor = conn.cursor()
-
+cursor.execute("SELECT id from zup_employees")
+ids = cursor.fetchall()
+ids_list = []
+for item in ids:
+    ids_list.append(item[0])
 for empl in root.iter('CatalogObject.ФизическиеЛица'):
     empl_id = empl.find('Code').text
-    fio = empl.find('ФИО').text
-    dob = empl.find('ДатаРождения').text
-    gender = empl.find('Пол').text
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO zup_employees (id, FIO, DOB, GENDER) VALUES (?, ?, ?,?)", (empl_id, fio, dob, gender))
-    conn.commit()
+    if empl_id not in ids_list:
+        fio = empl.find('ФИО').text
+        dob = empl.find('ДатаРождения').text
+        gender = empl.find('Пол').text
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO zup_employees (id, FIO, DOB, GENDER) VALUES (?, ?, ?,?)", (empl_id, fio, dob, gender))
+        conn.commit()
 conn.close()
