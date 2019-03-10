ac7b2d8cdeed6955649041e4157ff6324d5dbce2
blizzard <s41.blizzard@mail.ru>
2019-03-10 22:23:37 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	employees.sqlite

--- bd_connection/sqlite_connect.py
+++ bd_connection/sqlite_connect.py
@@ -4,8 +4,6 @@ import sqlite3
 def _sqlite_connect_and_query():
     conn = sqlite3.connect('employees.sqlite')
     cursor = conn.cursor()
-    cursor.execute(
-        "SELECT id, LastName, FirstName, Photo from employees WHERE strftime('%m',DOB) = strftime('%m','now') AND  strftime('%d','now') = strftime('%d', DOB)")
 
     results = cursor.fetchall()
 
---
+++ photo_uploader.py
@@ -0,0 +1,17 @@
+import sqlite3
+import os
+
+conn = sqlite3.connect('employees.sqlite')
+cursor = conn.cursor()
+cursor.execute('SELECT id,  FIO from zup_employees')
+results = cursor.fetchall()
+print(results)
+file_list = os.listdir(r'C:\Users\s41bl\PycharmProjects\project_birthday\empl_photo')
+
+file_names = [os.path.splitext(x)[0] for x in file_list]
+file_names_trimmed = []
+for i in file_names:
+    file_names_trimmed.append(i.strip())
+
+print(file_names_trimmed)
+
